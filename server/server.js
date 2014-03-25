50bc44183b4a60f20e4af9e7fa150078ec83de30
Dillon Grove <dillongrove@gmail.com>
2014-03-25 16:57:35 -0400

    Installed templating (handlebars), updated gitignore and package.json

--- server/server.js
+++ server/server.js
@@ -5,19 +5,31 @@ var express = require("express");
 var app = express();
 var constants = require("./constants.js");
 
+// This an npm module which includes references to a lot of templating engines
+var templatingEngines = require('consolidate');
+
 /* ---------- CONFIGURATION ------------------------------------------------- */
 
-app.configure(function(){ 
-    app.use(express.static(path.join(__dirname, "/static")));
-    app.set("port", process.env.PORT || 7000);
+app.configure(function(){
     app.use(express.bodyParser());
+    app.use(express.static(path.join(__dirname, "/static")));
+
+    // assign the handlebars templating engine to handle html files
+    app.engine('html', templatingEngines.handlebars);
+
+    app.set('view engine', 'html'); // set .html as the default extension
+    app.set('views', path.join(__dirname, 'templates')); // set view dir
+
+    app.set("port", process.env.PORT || 7000);
 });
 
 /* ---------- ROUTES -------------------------------------------------------- */
 
 // Home Route
 app.get('/', function(req, res) {
-    res.sendfile('static/index.html');
+    res.render('index.html', {
+        derp: "foo"
+    });
 });
 
 /* ---------- RUN SERVER ---------------------------------------------------- */
