66524b27784bf1515bff5f7afb0f92f0c3b0a4d6
Dillon Grove <dillongrove@gmail.com>
2014-03-25 23:37:20 -0400

    Switching to swig for templating, one file per dossier, reading in dossier via FS

--- server/server.js
+++ server/server.js
@@ -4,12 +4,10 @@ var path = require("path");
 var express = require("express");
 var app = express();
 var constants = require("./constants.js");
+var fs = require("fs");
 
 // This an npm module which includes references to a lot of templating engines
-var templatingEngines = require('consolidate');
-
-// Data
-var dossiers = require("./dossiers.js");
+var templatingEngines = require("consolidate");
 
 /* ---------- CONFIGURATION ------------------------------------------------- */
 
@@ -17,11 +15,11 @@ app.configure(function(){
     app.use(express.bodyParser());
     app.use(express.static(path.join(__dirname, "/static")));
 
-    // assign the handlebars templating engine to handle html files
-    app.engine('html', templatingEngines.handlebars);
+    // assign the swig templating engine to handle html files
+    app.engine("html", templatingEngines.swig);
 
-    app.set('view engine', 'html'); // set .html as the default extension
-    app.set('views', path.join(__dirname, 'templates')); // set view dir
+    app.set("view engine", "html"); // set .html as the default extension
+    app.set("views", path.join(__dirname, "templates")); // set view dir
 
     app.set("port", process.env.PORT || 7000);
 });
@@ -29,13 +27,37 @@ app.configure(function(){
 /* ---------- ROUTES -------------------------------------------------------- */
 
 // Home Route
-app.get('/', function(req, res) {
-    res.render('index.html', {
-        derp: "foo"
+app.get("/", function(req, res) {
+    getDossier(1, function(err, dossier){
+        console.log(dossier.basics.name);
+        res.render("index.html", dossier.basics.name);
     });
 });
+
+app.get("/dossier", function(req, res){
+    res.render("dossier.html", dossiers.dossier1);
+});
+
+
+function getDossier(num, callback){
+    fs.readFile("dossier" + num + ".json", function(err, data) {
+        if(err){
+            callback(err)
+        } else {
+            try {
+                var obj = JSON.parse(data);
+                callback(undefined, obj);
+            } catch(e) {
+                callback(e);
+                return; 
+            }
+        }
+    });
+}
 
 /* ---------- RUN SERVER ---------------------------------------------------- */
 
 app.listen(app.get("port"));
 console.log("Listening on " + app.get("port"));
+
+/* ---------- UTILS --------------------------------------------------------- */
