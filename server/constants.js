373047479017532d4da2f39b84ff9b6877d93ee8
Dillon Grove <dillongrove@gmail.com>
2014-03-31 02:47:57 -0400

    Removing server code from here so that it can be moved to another repository

--- server/constants.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    APP_NAME: "Dossier Prototype (Web Component)"
-}
--- server/static/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".info").on("click", function(e){
-        // find the meta info, if it exists
-        var meta = $(this).closest(".info_wrapper").find(".meta");
-
-        // EYGUHGHHGH javascript animations are bad :(
-        meta.slideToggle(200);
-    });
-})
